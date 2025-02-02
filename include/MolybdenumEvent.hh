3fc6ea586e97992911c03d95b504968b5281eaab
Ihor Melnyk <ihor.melnyk1337@gmail.com>
2025-02-02 11:30:53 +0200

    made a lot of changes; new commits start from here

--- include/MolybdenumEvent.hh
+++
@@ -1,8 +0,0 @@
-//
-// Created by ihor on 1/10/25.
-//
-
-#ifndef MOLYBDENUMEVENT_HH
-#define MOLYBDENUMEVENT_HH
-
-#endif // MOLYBDENUMEVENT_HH
--- include/MolybdenumRun.hh
+++
@@ -1,8 +0,0 @@
-//
-// Created by ihor on 1/10/25.
-//
-
-#ifndef MOLYBDENUMRUN_HH
-#define MOLYBDENUMRUN_HH
-
-#endif // MOLYBDENUMRUN_HH
